649040191e280555f11e53040f1a59d9eba878c8
devkaua13 <kaua.vinicios_2005@hotmail.com>
2026-06-12 21:48:50 -0300

    feat: implements first log

---
+++ src/lesson05.py
@@ -0,0 +1,15 @@
+import logging 
+
+format1 = "%(levelname)s|%(name)s|%(asctime)s|%(message)s|%(filename)s|%(lineno)d"
+
+logging.basicConfig(
+    level = logging.DEBUG,
+    format = format1,
+    filename = "log.log",
+    filemode = "a",
+    encoding = "utf-8",
+)
+
+logger = logging.getLogger("meuapp")
+
+logger.debug("Essa bosta que vai aparecer no arquivo quando rodar")
