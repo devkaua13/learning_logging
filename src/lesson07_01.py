a49f662ef41ec7ad60e36b9cf2da996d3529e2e9
devkaua13 <kaua.vinicios_2005@hotmail.com>
2026-06-16 19:59:19 -0300

    feat: finish class 07

--- src/lesson07_01.py
+++ src/lesson07_01.py
@@ -1,4 +1,7 @@
+import logging 
 from typing import Any
+
+from logging.config import dictConfig
 
 LOGGING_CONFIG: dict[str, Any] = {
     "version": 1,
@@ -6,19 +9,57 @@ LOGGING_CONFIG: dict[str, Any] = {
     "formatters": {
         "root_formatter":{
             "class": "logging.Formatter",
-            "format": "[ ROOT:%(levelname)s] %(message)s",
+            "format": "[ DICT ROOT:%(levelname)s] %(message)s",
         },
         "file_formatter":{
             "class": "logging.Formatter",
-            "format": "()"
+            "format": "%(levelname)s|%(name)s|%(asctime)s|%(message)s|%(filename)s|%(lineno)d",
         },
         "stream_formatter":{
             "class": "logging.Formatter",
+            "format": "[MEUAPP:%(levelname)s] %(message)s",
+        },  
+    },
+    "handlers":{
+        "root_stream_handler": {
+            "class": "logging.StreamHandler",
+            "formatter": "root_formatter",
+            "stream": "ext://sys.stdout",
+            "level": "WARNING",
+        },
+
+        "file_handler": {
+            "class": "logging.FileHandler",
+            "filename": "log.log",
+            "mode": "w",
+            "encoding": "utf-8",
+            "formatter": "file_formatter",
+        },
+
+        "stream_handler": {
+            "class": "logging.StreamHandler",
+            "formatter": "stream_formatter",
+            "stream": "ext://sys.stdout",
+        }
+    },
+    "root": {
+        "handlers": ["root_stream_handler"],
+    },
+    "loggers": {
+        "meuapp": {
+            "level": "DEBUG",
+            "handlers": ["file_handler", "stream_handler"],
         },
     },
-    "handlers":{},
-    "filters": {},
-    "root": {},
-    "loggers": {},
     
 }
+
+dictConfig(LOGGING_CONFIG)
+
+logger = logging.getLogger("meuapp")
+
+logger.debug("Mensagem de DEBUG")
+logger.info("Mensagem de INFO")
+logger.warning("Mensagem de WARNING")
+logger.error("Mensagem de ERROR")
+logger.critical("Mensagem de CRITICAL")
