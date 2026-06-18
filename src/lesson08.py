9d0362a6484f19295ebf916e91c5d4bec1d81d00
devkaua13 <kaua.vinicios_2005@hotmail.com>
2026-06-17 21:04:23 -0300

    fix: finally lesson08

--- src/lesson08.py
+++ src/lesson08.py
@@ -13,13 +13,13 @@ LOGGING_CONFIG: dict[str, Any] = {
             "format": (
                 "%(levelname)s|%(name)s|%(asctime)s|%(message)s|%(filename)s|%(lineno)d"
                 "|%(funcName)s|%(module)s|%(process)d|%(processName)s|%(thread)d|%(threadName)s"
-                "%(taskName)s|"
+                "%(taskName)s|%(operation)s"
             )   
         },
 
         "console":{
             "format": "%(message)s",
-            "datefmt": "%H:%M:%S"},
+            "datefmt": "[%X]"},
     },
 
     "handlers":{
@@ -27,11 +27,11 @@ LOGGING_CONFIG: dict[str, Any] = {
         "console":{
             "()": "rich.logging.RichHandler",
             "formatter": "console",
-            "rich_tracebacks": True,
-            "tracebacks_show_locals": True,
+            "rich_tracebacks": False,
+            "tracebacks_show_locals": False,
             "show_time": True,
             "show_level": True,
-            "omit_repeated_times": False,
+            "omit_repeated_times": True,
             "markup": False,
             "enable_link_path": True,
             "show_path": True,
@@ -61,16 +61,27 @@ dictConfig(LOGGING_CONFIG)
 
 logger = logging.getLogger("meuapp")
 
+
+# Não utilizar fstring, pois com args podemos salvar os valores em tempo real no log
 def dividir(x: float, y: float) -> float:
-    logger.debug(f"Recebi x={x} e y={y}")
+    logger.debug("Recebi x=%s e y=%s", x, y, extra={"operation": f"{x =} / {y =}"}
+    )
 
     if y == 0:
         msg = f"Não posso dividir {x} por {y}"
         raise ZeroDivisionError(msg)
 
     resultado = x / y
-    logger.info(f"O resultado é {resultado}")
+    
+    logger.info("O resultado da conta entre x=%s / y=%s = %s",x, y, resultado, extra={"operation": f"{x} / {y} = {resultado}"}
+    )
     return resultado
 
 if __name__ == '__main__':
     dividir(10, 5)
+
+
+#    try:
+#        dividir(10, 0)
+#    except ZeroDivisionError:
+#        logger.exception("Divisao por 0")
