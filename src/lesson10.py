2be31c3d1f8919cc5655efe566c7244d0ddbf124
devkaua13 <kaua.vinicios_2005@hotmail.com>
2026-06-25 21:18:04 -0300

    feat: finally lesson10

--- src/lesson10.py
+++ src/lesson10.py
@@ -1,4 +1,13 @@
 import config_logging
+import logging
 
 if __name__ == "__main__":
     config_logging.setup()
+    
+    logger = logging.getLogger('meuapp')
+
+    logger.debug("Mensagem de debug")
+    logger.info("Mensagem de info")
+    logger.warning("Mensagem de warning")
+    logger.error("Mensagem de error")
+    logger.critical("Mensagem de critical")
